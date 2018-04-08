c39e461373baf06173c34e9a1210727113644c23
Sebastian Hönel <development@hoenel.net>
2018-04-08 17:31:10 +0200

    Update index.js and also export Calendar-related types

--- index.js
+++ index.js
@@ -1,9 +1,11 @@
 const { Job, JobQueue, symbolRun, symbolDone, symbolFailed } = require('./lib/JobQueue')
 , { JobWithCost, JobQueueCapabilities } = require('./lib/JobQueueCapabilities')
-, { Progress, ProgressNumeric, symbolProgress } = require('./lib/Progress');
+, { Progress, ProgressNumeric, symbolProgress } = require('./lib/Progress')
+, { CalendarScheduler, Calendar, CalendarEventSimple, symbolCalendarEvent } = require('./lib/CalendarScheduler');
 
 module.exports = {
   Job, JobQueue, symbolRun, symbolDone, symbolFailed,
   JobWithCost, JobQueueCapabilities,
-  Progress, ProgressNumeric, symbolProgress
+  Progress, ProgressNumeric, symbolProgress,
+  CalendarScheduler, Calendar, CalendarEventSimple, symbolCalendarEvent
 };
