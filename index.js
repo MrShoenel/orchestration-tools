91bec88c8ea02e14bd5d3ca0cb0631f8a3c19a9b
Sebastian Hönel <sehoaa@lnu.se>
2018-04-10 20:00:36 +0200

    Export new classes BaseScheduler, Interval, IntervalScheduler and symbolIntervalEvent in index.js and increase version to v1.6.0

--- index.js
+++ index.js
@@ -1,11 +1,14 @@
 const { Job, JobQueue, symbolRun, symbolDone, symbolFailed } = require('./lib/JobQueue')
 , { JobWithCost, JobQueueCapabilities } = require('./lib/JobQueueCapabilities')
 , { Progress, ProgressNumeric, symbolProgress } = require('./lib/Progress')
-, { CalendarScheduler, Calendar, CalendarEventSimple, symbolCalendarEvent } = require('./lib/CalendarScheduler');
+, { CalendarScheduler, Calendar, CalendarEventSimple, symbolCalendarEvent } = require('./lib/CalendarScheduler')
+, { BaseScheduler } = require('./lib/BaseScheduler')
+, { Interval, IntervalScheduler, symbolIntervalEvent } = require('./lib/IntervalScheduler');
 
 module.exports = {
   Job, JobQueue, symbolRun, symbolDone, symbolFailed,
   JobWithCost, JobQueueCapabilities,
   Progress, ProgressNumeric, symbolProgress,
-  CalendarScheduler, Calendar, CalendarEventSimple, symbolCalendarEvent
+  CalendarScheduler, Calendar, CalendarEventSimple, symbolCalendarEvent,
+  BaseScheduler, Interval, IntervalScheduler, symbolIntervalEvent
 };
